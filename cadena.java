4fb5d093734abdf11c878394e3afa36bb1dfc88f
LOREGCH <lorena.checa098@gmail.com>
2021-07-09 14:48:55 -0500

    primer archivo

---
+++ cadena.java
@@ -0,0 +1,71 @@
+/**
+ * cadena
+ */
+public class cadena {
+
+    public static void main(String[] args) {
+        /** String s1 = "abcdef";
+        int a=s1.length();
+        System.out.println("longitud de la cadena");
+        System.out.println(a);
+        String s2=s1.toUpperCase();
+        System.out.println("La cadena en Mayusculas "+s2);
+        System.out.println("Posición de la F");
+        System.out.println(s2.indexOf('F'));
+        System.out.println("Caracter en la posicion tres");
+        System.out.println(s1.charAt(3));
+        Boolean slida=s1.equalsIgnoreCase(s2);
+        System.out.println(slida);
+         */
+        /** 
+        int i = -25;
+        int j = -9;
+        double x = 72.3543;
+        double y = 0.3498;
+
+        System.out.println("|" + i + "| es " + Math.abs(i));
+        */
+        //Funciones trigonométricas
+        /** double angulo = 272.2* Math.PI/180.0;
+        System.out.println("cos(" + angulo + ") es " + Math.cos(angulo));
+        System.out.println("sin(" + angulo + ") es " + Math.sin(angulo));
+        System.out.println("tan(" + angulo + ") es " + Math.tan(angulo));
+        */
+
+        //Funciones exponencial y logarítmica, exp e elevado a una potencia
+        
+        /**System.out.println("exp(15) es " +  Math.exp(15));
+        System.out.println("exp(0.0) es " +  Math.exp(0.0));
+        */
+        /**
+        //log calcula el logaritmo natural (de base e) de un número
+        System.out.println("log(1.0) es " + Math.log(1.0));
+         */
+        
+         /** 
+        //potencia y raíz cuadrada se emplea pow(x, y)
+        System.out.println("pow(10.0, 3.5) es " +  Math.pow(10.0,3.5));
+        System.out.println("La raíz cuadrada de " + x + " es " + Math.sqrt(x));
+        */
+        /**
+        System.out.println(x + " es aprox. " + (double)Math.round(x*100)/100);
+        System.out.println(y + " es aprox. " + (double)Math.round(y*100)/100);
+        //con la función floor en vez de round
+
+        System.out.println(x + " es aprox. " + Math.floor(x*100)/100);
+        System.out.println(y + " es aprox. " + Math.floor(y*100)/100);
+        //El método round redondea siempre al entero más próximo,3.6 a 4, -3.6 a -4
+        //El método floor devuelve el entero menor, 3.9 a 3.0 y -3.9 a -3.0. 
+        */
+       
+        /** 
+        // para hallar el menor de dos número
+        System.out.println("min(" + i + "," + j + ") es " + Math.min(i,j));
+        System.out.println("min(" + x + "," + y + ") es " + Math.min(x,y));
+        // Para hallar el mayor de dos números
+        System.out.println("max(" + i + "," + j + ") es " + Math.max(i,j));
+        */
+        //Wrappers java son utilizados para convertir cadenas de caracteres (texto) en números.
+        //cuando se leen valores desde el teclado, desde un fichero de texto
+        }
+}
